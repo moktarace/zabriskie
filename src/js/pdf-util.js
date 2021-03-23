1f7f2db7b4f2d86d9e95eea4e50bbe076a05afd2
moktarace <lejazzman1@gmail.com>
2021-03-23 22:13:31 +0000

    Mise a jour des numero de contrats

--- src/js/pdf-util.js
+++ src/js/pdf-util.js
@@ -21,11 +21,19 @@ export async function generatePdf(profile) {
   pdfDoc.setCreator('')
   const page1 = pdfDoc.getPages()[0]
 
+  const clientNumber = randomIntFromInterval(10000000, 99999999).toString();
+  const contractNumber = randomIntFromInterval(100000000000, 999999999999).toString();
+  const subNumber = contractNumber.substr(0, 8) + "E";
+
+
   const addressFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
   const nameFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold)
   const textFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
   const drawAddressText = (text, x, y, size = 10) => {
     page1.drawText(text, { x, y, size, addressFont })
+  }
+  const drawContractText = (text, x, y, size = 9) => {
+    page1.drawText(text, { x, y, size, nameFont })
   }
   const drawNameText = (text, x, y, size = 10) => {
     page1.drawText(text, { x, y, size, nameFont })
@@ -39,6 +47,10 @@ export async function generatePdf(profile) {
   drawAddressText(`${address.toUpperCase()}`, 300, 635)
   drawAddressText(`${zipcode} ${city.toUpperCase()}`, 300, 620)
 
+  drawContractText(`${clientNumber}`, 189, 568)
+  drawContractText(`${subNumber}`, 189, 558)
+  drawContractText(`${contractNumber}`, 189, 548)
+
   drawNameText(`${lastname.toUpperCase()} ${firstname.toUpperCase()}`, 42, 453)
 
   drawText(`${address.toUpperCase()}, ${zipcode} ${city.toUpperCase()}.`, 42, 369)
@@ -50,6 +62,9 @@ export async function generatePdf(profile) {
   return new Blob([pdfBytes], { type: 'application/pdf' })
 }
 
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function getIdealFontSize(font, text, maxWidth, minSize, defaultSize) {
   let currentSize = defaultSize
